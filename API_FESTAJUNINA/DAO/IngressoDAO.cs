a4645752152b0db3dc7d50bf17ea944fc52822f2
Lucas Unzer <unzerlucas2006@gmail.com>
2024-04-03 10:39:22 -0300

    finaly api ingresso

--- API_FESTAJUNINA/DAO/IngressoDAO.cs
+++ API_FESTAJUNINA/DAO/IngressoDAO.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using API_FESTAJUNINA.Models;
 using API_FESTAJUNINA.Repository;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 
 namespace API_FESTAJUNINA.DAO
 {
@@ -58,11 +61,142 @@ namespace API_FESTAJUNINA.DAO
             }
             return ingressos;
         }
+        public Ingresso GetId(int id)
+        {
+            Ingresso ingresso = new Ingresso();
+            string query = $"select * from ingresso where id_ingresso = {id}";
+
+            try
+            {
+                _connection.Open();
+                MySqlCommand command = new MySqlCommand(query, _connection);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        ingresso.IdIngresso = reader.GetInt32("id_ingresso");
+                        ingresso.Valor = reader.GetString("valor");
+                        ingresso.Status = reader.GetString("status");
+                        ingresso.Tipo = reader.GetString("tipo");
+                        ingresso.CodigoQr = reader.GetString("codigo_qr");
+                        ingresso.UsuarioId = reader.GetInt32("usuario_id_usuario");
+                        ingresso.UsuarioPedido = reader.GetInt32("usuario_pedido_id_usuario");
+                        ingresso.LotesId = reader.GetInt32("lotes_id_lotes");
+
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro de Banco: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro Desconhecido: {ex.Message}");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return ingresso;
+        }
 
         public void CriarIngresso(Ingresso ingresso)
         {
             string query = "INSERT TO ingresso (id_ingresso,valor, status, tipo, codigo-qr, usuario_id_usuario, usuario_pedido_id_usuario, lotes_id_lotes)" +
                            "values (@IdIngresso, @Valor, @Status, @Tipo, @CodigoQr, @UsuarioId, @UsuarioPedido, @LotesId)";
+
+            Console.WriteLine(Guid.NewGuid());
+            try
+            {
+                _connection.Open();
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    command.Parameters.AddWithValue("@Valor", ingresso.Valor);
+                    command.Parameters.AddWithValue("@Status", "RESERVADO");
+                    command.Parameters.AddWithValue("@Tipo", ingresso.Tipo);
+                    command.Parameters.AddWithValue("@CodigoQr", Guid.NewGuid().ToString());
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro de Banco: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro Desconhecido: {ex.Message}");
+            }
+            finally
+            {
+                _connection.Close();
+            }
         }
+        public void ValidarIngresso(int id, Ingresso ingresso)
+        {
+            string query = "UPDATE ingresso SET" +
+                           "valor=@Valor," +
+                           "status=@Status" +
+                           "tipo=@Tipo," +
+                           "codigo_qr=@CodigoQr" +
+                           "WHERE id_ingresso=@IdIngresso";
+            try
+            {
+                _connection.Open();
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    command.Parameters.AddWithValue("@IdIngresso", ingresso.IdIngresso);
+                    command.Parameters.AddWithValue("@Valor", ingresso.Valor);
+                    command.Parameters.AddWithValue("@Status", "RESERVADO");
+                    command.Parameters.AddWithValue("@Tipo", ingresso.Tipo);
+                    command.Parameters.AddWithValue("@CodigoQr", Guid.NewGuid().ToString());
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro de Banco: {ex.Message}");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro Desconhecido: {ex.Message}");
+            }
+
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        public void DeletarIngresso(int id)
+        {
+            string query = "DELETE FROM ingresso WHERE id_ingresso = @IdIngresso";
+
+            try
+            {
+                _connection.Open();
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    command.Parameters.AddWithValue("@IdIngresso", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro de Banco: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro Desconhecido: {ex.Message}");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+
     }
 }
